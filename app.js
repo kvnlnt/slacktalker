75c179d3173508892e258409cab033238fb04d41
Kevin Lint <kevinlint@toolhouse.com>
2017-07-05 15:44:43 -0700

    emoji preview

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const alpha = require('./alpha.json');
+const emoticons = require('./emoticons');
 const package = require('./package');
 const commander = require('commander');
 const chalk = require('chalk');
@@ -18,20 +19,39 @@ commander
         var row;
         var letter;
         words.forEach(function (word) {
-            word = word.replace(/[^a-zA-Z]+/g,"").toUpperCase();
+            word = word.replace(/[^a-zA-Z]+/g, "")
+                .toUpperCase();
 
             console.log("\n", chalk.red(word), "\n");
 
-            // command line version
-            for (var i = 0; i < 7; i++) {
-                row = "";
-                for (var j = 0; j < word.length; j++) {
-                    var letter = alpha[word[j]][i];
-                    if (j > 0) letter = letter.substring(1); // compress letters
-                    row += letter;
+            // preview version
+            try {
+                for (var i = 0; i < 7; i++) {
+                    row = "";
+                    for (var j = 0; j < word.length; j++) {
+                        var letter = alpha[word[j]][i];
+                        if (j > 0) letter = letter.substring(1); // compress letters
+                        letter = letter.replace(/#/g, options.primaryChar)
+                            .replace(/-/g, options.secondaryChar); // replace with slack chars
+                        row += letter;
+                    }
+                    var previewRow = row.replace(new RegExp(options.primaryChar, "g"), "\\u" + emoticons[options.primaryChar])
+                        .replace(new RegExp(options.secondaryChar, "g"), "\\u" + emoticons[options.secondaryChar]);
+                    console.log(eval("'" + previewRow + "'"));
                 }
-                console.log(row);
+            } catch (err) {
+                console.log("Unfortunately one of your emoticons isn't available for preview, so here's the default preview: \n");
+                for (var i = 0; i < 7; i++) {
+                    row = "";
+                    for (var j = 0; j < word.length; j++) {
+                        var letter = alpha[word[j]][i];
+                        if (j > 0) letter = letter.substring(1); // compress letters
+                        row += letter;
+                    }
+                    console.log(row);
+                }
             }
+
 
             console.log();
 
